c873292c0e3c736bc5bf5d0aea122f80a9e707b3
AnimaVR <22724774+AnimaVR@users.noreply.github.com>
2024-02-23 22:57:26 +0000

    hmmpf

--- _BPE/DataCreator/TokenizeandBin.cs
+++ _BPE/DataCreator/TokenizeandBin.cs
@@ -26,8 +26,8 @@ namespace BytePairEncoding
 
                 var writeTasks = new Task[]
                 {
-            WriteIdsToBinFileAsync("train.bin", trainWords),
-            WriteIdsToBinFileAsync("val.bin", valWords)
+            TrainNValSplitter.WriteIdsToBinFileWithPaddingAsync("train.bin", trainWords),
+            TrainNValSplitter.WriteIdsToBinFileWithPaddingAsync("val.bin", valWords)
                 };
                 await Task.WhenAll(writeTasks);
 
@@ -36,63 +36,5 @@ namespace BytePairEncoding
                 return valWords;
             }
         }
-
-        private static async Task WriteIdsToBinFileAsync(string fileName, int[] ids)
-        {
-            using (FileStream fs = new (fileName, FileMode.Create, FileAccess.Write, FileShare.None, bufferSize: 4096, useAsync: true))
-            using (BinaryWriter writer = new (fs))
-            {
-                foreach (int id in ids)
-                {
-                    writer.Write(id);
-                }
-                await fs.FlushAsync();
-            }
-        }
-
-        public static async Task WriteIdsToBinFileWithPaddingAsync(string fileName, int[] ids, int chunkSizeInBytes = 4096, int paddingToken = -1)
-        {
-            int tokenSizeInBytes = 4; // Size of int in bytes
-            int tokensPerChunk = chunkSizeInBytes / tokenSizeInBytes;
-            int totalTokens = ids.Length;
-            int totalFullChunks = totalTokens / tokensPerChunk;
-            int remainingTokens = totalTokens % tokensPerChunk;
-
-            FileStream stream = new (fileName, FileMode.Create, FileAccess.Write);
-            using (BinaryWriter writer = new (stream))
-            {
-                // Write full chunks
-                for (int i = 0; i < totalFullChunks; i++)
-                {
-                    for (int j = 0; j < tokensPerChunk; j++)
-                    {
-                        int tokenIndex = i * tokensPerChunk + j;
-                        writer.Write(ids[tokenIndex]);
-                    }
-                }
-
-                // Write remaining tokens, if any
-                if (remainingTokens > 0)
-                {
-                    for (int i = 0; i < remainingTokens; i++)
-                    {
-                        int tokenIndex = totalFullChunks * tokensPerChunk + i;
-                        writer.Write(ids[tokenIndex]);
-                    }
-
-                    // Pad the last chunk
-                    int paddingTokensNeeded = tokensPerChunk - remainingTokens;
-                    for (int i = 0; i < paddingTokensNeeded; i++)
-                    {
-                        writer.Write(paddingToken);
-                    }
-                }
-            }
-
-            await stream.FlushAsync();
-            stream.Close();
-        }
-
-
     }
 }
