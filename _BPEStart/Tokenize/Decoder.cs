2172a84c7b5a09790299286b5619bf8834fa92ad
AnimaVR <22724774+AnimaVR@users.noreply.github.com>
2024-02-23 22:14:44 +0000

    On a roll with this refactor, apparently.

--- _BPEStart/Tokenize/Decoder.cs
+++ _BPEStart/Tokenize/Decoder.cs
@@ -1,5 +1,4 @@
-﻿using System.Collections;
-using System.Collections.Generic;
+﻿using System.Collections.Generic;
 using System.Linq;
 
 namespace BytePairEncoding
@@ -7,17 +6,22 @@ namespace BytePairEncoding
     public class Decoder
     {
         private readonly BPE _bpe;
+        DecodedTokenExpander _expander;
 
         public Decoder(BPE bpe)
         {
             _bpe = bpe;
+            _expander = new DecodedTokenExpander(_bpe);
         }
 
         public string Decode(int[] ids)
         {
             var tokens = ConvertIdsToTokens(ids);
-            tokens = ExpandTokens(tokens);
+
+            tokens = _expander.ExpandTokens(tokens);
+
             tokens.RemoveAll(token => token == "<PAD>");
+
             return string.Join("", tokens);
         }
 
@@ -26,49 +30,5 @@ namespace BytePairEncoding
             return ids.Select(id => _bpe.token2id.FirstOrDefault(kv => kv.Value == id).Key ?? "<UNK>").ToList();
         }
 
-        private List<string> ExpandTokens(List<string> tokens)
-        {
-            for (int i = 0; i < tokens.Count;)
-            {
-                switch (tokens[i])
-                {
-                    case "<SPACE>":
-                        tokens[i] = " ";
-                        i++;
-                        break;
-                    case "<UNK>":
-                        tokens[i] = "";
-                        i++;
-                        break;
-                    case "<NEWLINE>":
-                        tokens[i] = "\n";
-                        i++;
-                        break;
-                    default:
-                        i = TryExpandToken(tokens, i);
-                        break;
-                }
-            }
-
-            return tokens;
-        }
-
-        private int TryExpandToken(List<string> tokens, int index)
-        {
-            foreach (DictionaryEntry mergePair in _bpe.mergePairs)
-            {
-                if (mergePair.Value != null && (string)mergePair.Value == tokens[index])
-                {
-                    string originalPair = (string)mergePair.Key;
-                    if (!string.IsNullOrEmpty(originalPair))
-                    {
-                        tokens[index] = originalPair[0].ToString();
-                        tokens.Insert(index + 1, originalPair.Substring(1));
-                        return index + 2; 
-                    }
-                }
-            }
-            return index + 1;
-        }
     }
 }
--- _BPEStart/Tokenize/Encoder.cs
+++ _BPEStart/Tokenize/Encoder.cs
@@ -1,18 +1,16 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using System.Linq;
+﻿using System.Collections.Generic;
 
 namespace BytePairEncoding
 {
     public class Encoder
     {
         public BPE _bpe;
-
+        EncodedWordProcessor _wordprocessor;
 
         public Encoder(BPE bpe)
         {
             _bpe = bpe;
-            
+            _wordprocessor = new EncodedWordProcessor(_bpe);
         }
 
         public int[] Encode(string text)
@@ -23,102 +21,15 @@ namespace BytePairEncoding
 
             foreach (var word in words)
             {
-                List<int> wordTokens = ProcessWord(word);
+                List<int> wordTokens = _wordprocessor.ProcessWord(word);
                 encodedTokens.AddRange(wordTokens);
-                AddSpaceToken(encodedTokens);
+                _wordprocessor.AddSpaceToken(encodedTokens);
             }
 
-            RemoveTrailingSpace(encodedTokens);
+            _wordprocessor.RemoveTrailingSpace(encodedTokens);
 
             return encodedTokens.ToArray();
-        }
-
-
-        private List<int> ProcessWord(string word)
-        {
-            List<string> wordList = word.Select(ch => ch.ToString()).ToList();
-            List<int> encodedTokens = new ();
-
-            string wordJoined = string.Join("", wordList);
-
-            for (int i = 0; i < wordList.Count; i++)
-            {
-                var (bestToken, bestLength) = FindBestToken(wordJoined, i);
-
-                HandleSpecialTokens(ref bestToken);
-                ReplaceUnknownToken(bestToken);
-
-                int tokenValue = _bpe.token2id.FirstOrDefault(kv => kv.Key == bestToken).Value;
-                encodedTokens.Add(tokenValue);
-
-                i += bestLength - 1;
-            }
-
-            return encodedTokens;
-        }
-
-        private void AddSpaceToken(List<int> encodedTokens)
-        {
-            var spaceTokenValue = _bpe.token2id.FirstOrDefault(kv => kv.Key == "<SPACE>").Value;
-            if (spaceTokenValue != default)
-            {
-                encodedTokens.Add(spaceTokenValue);
-            }
-        }
-
-        private void RemoveTrailingSpace(List<int> encodedTokens)
-        {
-            var spaceTokenValue = _bpe.token2id.FirstOrDefault(kv => kv.Key == "<SPACE>").Value;
-            if (encodedTokens.Count > 0 && encodedTokens[^1] == spaceTokenValue)
-            {
-                encodedTokens.RemoveAt(encodedTokens.Count - 1);
-            }
-        }
-
-
-        private (string, int) FindBestToken(string wordJoined, int startIdx)
-        {
-            string bestToken = wordJoined[startIdx].ToString();
-            int bestLength = 1;
-
-            foreach (DictionaryEntry pair in _bpe.mergePairs)
-            {
-
-                var key = pair.Key as string; 
-                if (key == null) continue; 
-
-                if (key.Length > bestLength && startIdx + key.Length <= wordJoined.Length)
-                {
-                    string substring = wordJoined.Substring(startIdx, key.Length);
-                    if (substring == key)
-                    {
-                        bestLength = key.Length;
-                        bestToken = pair.Value as string ?? ""; 
-                    }
-                }
-            }
-
-            return (bestToken, bestLength);
-        }
-
-
-        private static void HandleSpecialTokens(ref string token)
-        {
-            if (token == "\n")
-            {
-                token = "<NEWLINE>";
-            }
-        }
-
-        private void ReplaceUnknownToken(string token)
-        {
-            if (!_bpe.token2id.Any(kv => kv.Key == token))
-            {
-                var unkTokenValue = _bpe.token2id.FirstOrDefault(kv => kv.Key == "<UNK>").Value;
-
-                _bpe.token2id.Add(new KeyValuePair<string, int>(token, unkTokenValue));
-            }
-        }
+        } 
 
     }
 }
