2e39fb4ba8278bd09cede534635ab7a326998024
AnimaVR <22724774+AnimaVR@users.noreply.github.com>
2024-02-24 14:12:26 +0000

    Refactor

---
+++ _BPE/Tokenize/Encoder.cs
@@ -0,0 +1,56 @@
+﻿using System.Collections.Generic;
+
+namespace BytePairEncoding
+{
+    public class Encoder
+    {
+        public BPE _bpe;
+
+        EncodedSequenceProcessor _sequenceprocessor;
+
+        // private readonly EncodedWordProcessor _wordprocessor;
+
+        public Encoder(BPE bpe)
+        {
+            _bpe = bpe;
+
+            _sequenceprocessor = new EncodedSequenceProcessor(_bpe);
+
+            //     _wordprocessor = new EncodedWordProcessor(_bpe);
+        }
+
+
+        // this is a proper char level encoder that uses spaces properly.
+
+        public int[] Encode(string text)
+        {
+            List<int> encodedTokens = _sequenceprocessor.ProcessSequence(text);
+            return encodedTokens.ToArray();
+        }
+
+
+        // this encoder uses spaces as NOT part of the token (<SPACE> is its own special thing)
+
+        /*
+        public int[] Encode(string text)
+        {
+            string[] words = text.Split(' ');
+
+            List<int> encodedTokens = new ();
+
+            foreach (var word in words)
+            {
+                List<int> wordTokens = _wordprocessor.ProcessWord(word);
+
+                encodedTokens.AddRange(wordTokens);
+
+                _wordprocessor.AddSpaceToken(encodedTokens);
+            }
+
+            _wordprocessor.RemoveTrailingSpace(encodedTokens);
+
+            return encodedTokens.ToArray();
+        } 
+        */
+    }
+}
